e0acd2950794d3de809529c3ebae66cf12afc611
MukkuMayc <st063775@student.spbu.ru>
2020-04-19 00:22:38 +0300

    Update graph

--- include/graph.hpp
+++ include/graph.hpp
@@ -9,44 +9,63 @@ using std::set;
 class Node {
     uint64_t _id;
 public:
-    explicit Node(uint64_t id): _id(id) {}
+    explicit Node(uint64_t id) : _id(id) {}
 
-    bool operator<(const Node& other) const {
+    bool operator<(const Node &other) const {
         return _id < other._id;
+    }
+
+    [[nodiscard]]
+    uint64_t id() const {
+        return _id;
     }
 };
 
 class Edge {
-    Node& _from;
-    Node& _to;
+    uint64_t _from;
+    uint64_t _to;
 public:
-    explicit Edge(Node& from, Node& to): _from(from), _to(to) {}
+    explicit Edge(Node &from, Node &to) : _from(from.id()), _to(to.id()) {}
 
-    bool operator<(const Edge& other) const {
-        return _from < other._from
-        ? true
-        : other._from < _from
-        ? false
-        : _to < other._to;
+    explicit Edge(uint64_t from, uint64_t to) : _from(from), _to(to) {}
+
+    bool operator<(const Edge &other) const;
+
+    [[nodiscard]]
+    uint64_t from() const {
+        return _from;
+    }
+
+    [[nodiscard]]
+    uint64_t to() const {
+        return _to;
     }
 };
+
+std::ostream &operator<<(std::ostream &out, const Edge &edge);
 
 class Graph {
     set<Node> _nodes;
     set<Edge> _edges;
 public:
-    explicit Graph(): _nodes(set<Node>()), _edges(set<Edge>()) {}
-    const Node& add_node(uint64_t id);
-    void add_edge(Node& from, Node& to);
+    explicit Graph() : _nodes(set<Node>()), _edges(set<Edge>()) {}
+
+    bool add_node(uint64_t id);
+
+    bool add_edge(uint64_t from, uint64_t to);
 
     [[nodiscard]]
-    const set<Node>& nodes() const;
+    const set<Node> &nodes() const {
+        return _nodes;
+    }
+
 
     [[nodiscard]]
-    const set<Edge>& edges() const;
+    const set<Edge> &edges() const {
+        return _edges;
+    }
 
-    static Graph from_map(osmium::io::File&);
-
-
+    static Graph from_map(osmium::io::File &);
 };
+
 #endif //GRAPHS_GRAPH_HPP
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,59 +1,66 @@
-#include "graph.hpp"
+#include <ostream>
 #include <cstdint>
 #include <set>
 #include <osmium/io/pbf_input.hpp>
 #include <osmium/handler.hpp>
 #include <osmium/visitor.hpp>
 
-const Node& Graph::add_node(uint64_t id) {
-    return *_nodes.emplace(id).first;
+#include "graph.hpp"
+
+std::ostream &operator<<(std::ostream &out, const Edge &edge) {
+    out << "Edge: " << "(" << edge.from() << ", " << edge.to() << ")" << std::endl;
+    return out;
 }
 
-void Graph::add_edge(Node& from, Node& to) {
-    _edges.emplace(from, to);
+bool Edge::operator<(const Edge &other) const {
+    return _from < other._from
+           ? true
+           : other._from < _from
+             ? false
+             : _to < other._to;
 }
 
-
-const set<Node>& Graph::nodes() const {
-    return _nodes;
+bool Graph::add_node(uint64_t id) {
+    return _nodes.emplace(id).second;
 }
 
-const set<Edge>& Graph::edges() const {
-    return _edges;
+bool Graph::add_edge(uint64_t from, uint64_t to) {
+    return _edges.emplace(from, to).second;
 }
 
-Graph Graph::from_map(osmium::io::File& input_file) {
-    struct CountHandler: public osmium::handler::Handler {
-        std::map<uint64_t, uint64_t> nodesCounter = std::map<uint64_t, uint64_t>();
-//            Graph& graph;
-        void way(const osmium::Way& way) noexcept {
-            for (const osmium::NodeRef& nr : way.nodes()) {
-                if (nodesCounter.contains(nr.positive_ref())) {
-                    uint64_t val = nodesCounter.find(nr.positive_ref())->second;
-                    nodesCounter.insert_or_assign(nr.positive_ref(), val + 1);
+Graph Graph::from_map(osmium::io::File &input_file) {
+    struct CountHandler : public osmium::handler::Handler {
+        std::map<uint64_t, uint64_t> nodes_counter = std::map<uint64_t, uint64_t>();
+
+        void way(const osmium::Way &way) noexcept {
+            for (const osmium::NodeRef &nr : way.nodes()) {
+                if (nodes_counter.contains(nr.positive_ref())) {
+                    uint64_t val = nodes_counter.find(nr.positive_ref())->second;
+                    nodes_counter.insert_or_assign(nr.positive_ref(), val + 1);
                 } else {
-                    nodesCounter.insert({nr.positive_ref(), 1});
+                    nodes_counter.insert({nr.positive_ref(), 1});
                 }
             }
         }
     };
 
-    struct AddEdgesHandler: public osmium::handler::Handler {
-        CountHandler& count_handler;
-        Graph& graph;
-        void way(const osmium::Way& way) noexcept {
+    struct AddEdgesHandler : public osmium::handler::Handler {
+        CountHandler &count_handler;
+        Graph &graph;
+
+        void add_edge_with_nodes(uint64_t from, uint64_t to) {
+            graph.add_node(from);
+            graph.add_node(to);
+            graph.add_edge(from, to);
+        }
+
+        void way(const osmium::Way &way) noexcept {
             auto prev = way.nodes().begin();
             for (const osmium::NodeRef &nr : way.nodes()) {
-                if (count_handler.nodesCounter.find(nr.positive_ref())->second > 1 &&
-                    nr != *way.nodes().begin()) {
-                    auto from = graph.add_node(prev->positive_ref());
-                    auto to = graph.add_node(nr.positive_ref());
-                    graph.add_edge(from, to);
-                    prev = &nr;
-                } else if (nr == *way.nodes().crbegin()) {
-                    auto from = graph.add_node(prev->positive_ref());
-                    auto to = graph.add_node(nr.positive_ref());
-                    graph.add_edge(from, to);
+                if ((count_handler.nodes_counter.find(nr.positive_ref())->second > 1 &&
+                     nr != *way.nodes().begin()) ||
+                    nr == *way.nodes().crbegin()) {
+                    add_edge_with_nodes(prev->positive_ref(), nr.positive_ref());
                     prev = &nr;
                 }
             }
@@ -63,13 +70,11 @@ Graph Graph::from_map(osmium::io::File& input_file) {
     Graph graph;
 
     osmium::io::Reader count_reader{input_file};
-    osmium::io::Reader add_edges_reader{input_file};
-
-
     CountHandler count_handler;
     osmium::apply(count_reader, count_handler);
     count_reader.close();
 
+    osmium::io::Reader add_edges_reader{input_file};
     AddEdgesHandler add_edges_handler{{}, count_handler, graph};
     osmium::apply(add_edges_reader, add_edges_handler);
     add_edges_reader.close();
