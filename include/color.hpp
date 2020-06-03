ce3318b4aeaef2dcbe40ea4683aa1ae6122d9597
MukkuMayc <st063775@student.spbu.ru>
2020-06-03 22:32:29 +0300

    Add colors generator

---
+++ include/color.hpp
@@ -0,0 +1,46 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#include <vector>
+#include <string>
+
+struct Color {
+/*
+ * 0 <= r <= 1
+ * 0 <= g <= 1
+ * 0 <= b <= 1
+ * 0 <= h <= 360
+ * 0 <= s <= 1
+ * 0 <= v <= 1
+ */
+    double& r() { return components[0]; }
+    double& g() { return components[1]; }
+    double& b() { return components[2]; }
+
+    double& h() { return components[0]; }
+    double& s() { return components[1]; }
+    double& v() { return components[2]; }
+
+    [[nodiscard]] std::string hex();
+    double components[3] { 0, 0, 0 };
+};
+
+using Colors = std::vector<Color>;
+
+/**
+ * Convert hsv to rgb
+ * Source: https://gist.github.com/fairlight1337/4935ae72bcbcc1ba5c72
+ * @param hsv
+ * @return
+ */
+auto hsv_to_rgb(Color hsv) -> Color;
+
+/**
+ * Generate n random colors in rgb format
+ * Source: https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+ * @param n
+ * @return vector of colors
+ */
+auto generate_colors(size_t n, double s = 0.5, double v = 0.95) -> Colors;
+
+#endif //COLOR_HPP
---
+++ src/color.cpp
@@ -0,0 +1,80 @@
+#include "color.hpp"
+#include <cmath>
+#include <random>
+#include <limits>
+#include <ctime>
+#include <sstream>
+
+std::string Color::hex() {
+    if (r() < 0 or g() < 0 or b() < 0 or r() > 1 or g() > 1 or b() > 1) {
+        return std::string { "000000" };
+    }
+
+    int red = static_cast<int>(r() * 256);
+    int green = static_cast<int>(g() * 256);
+    int blue = static_cast<int>(b() * 256);
+    std::stringstream stream;
+    stream << std::hex << red << green << blue;
+    return stream.str();
+}
+
+auto hsv_to_rgb(Color hsv) -> Color {
+    Color rgb;
+
+    double fC = hsv.v() * hsv.s(); // Chroma
+    double fHPrime = fmod(hsv.h() / 60.0, 6);
+    double fX = fC * (1 - fabs(fmod(fHPrime, 2) - 1));
+    double fM = hsv.v() - fC;
+
+    if (0 <= fHPrime && fHPrime < 1) {
+        rgb.r() = fC;
+        rgb.g() = fX;
+        rgb.b() = 0;
+    } else if (1 <= fHPrime && fHPrime < 2) {
+        rgb.r() = fX;
+        rgb.g() = fC;
+        rgb.b() = 0;
+    } else if (2 <= fHPrime && fHPrime < 3) {
+        rgb.r() = 0;
+        rgb.g() = fC;
+        rgb.b() = fX;
+    } else if (3 <= fHPrime && fHPrime < 4) {
+        rgb.r() = 0;
+        rgb.g() = fX;
+        rgb.b() = fC;
+    } else if (4 <= fHPrime && fHPrime < 5) {
+        rgb.r() = fX;
+        rgb.g() = 0;
+        rgb.b() = fC;
+    } else if (5 <= fHPrime && fHPrime < 6) {
+        rgb.r() = fC;
+        rgb.g() = 0;
+        rgb.b() = fX;
+    } else {
+        rgb.r() = 0;
+        rgb.g() = 0;
+        rgb.b() = 0;
+    }
+
+    rgb.r() += fM;
+    rgb.g() += fM;
+    rgb.b() += fM;
+
+    return rgb;
+}
+
+const double golden_ratio_conjugate = 0.618033988749895;
+auto generate_colors(size_t n, double s, double v) -> Colors {
+    std::default_random_engine gen((std::random_device()()));
+    std::uniform_int_distribution<uint64_t> dist(0, std::numeric_limits<uint64_t>::max());
+    double h = fmod(dist(gen), 360);
+
+    Colors colors;
+    colors.reserve(n);
+    for (size_t i = 0; i < n; ++i) {
+        colors.emplace_back(hsv_to_rgb(Color { h, s, v }));
+        h = fmod(h + golden_ratio_conjugate * 360., 360);
+    }
+
+    return colors;
+}
